96c745546bc93efd656615c22c6bc167c141595a
Steve Waldman <swaldman@mchange.com>
2023-07-30 05:16:00 -0400

    Grrr...

--- main/com/mchange/unifyrss/core.scala
+++ main/com/mchange/unifyrss/core.scala
@@ -12,7 +12,7 @@ class UnifyRssException( message : String, cause : Throwable = null ) extends Ex
 class IncompatibleDuplicateBindings(bindings : immutable.Set[(String,String)]) extends UnifyRssException(s"Incompatible duplicate bindings! ${bindings}", null)
 class BadItemXml(message : String, cause : Throwable = null)                   extends UnifyRssException( message, cause )
 class BadAtomXml(message : String, cause : Throwable = null)                   extends UnifyRssException( message, cause )
-class RssFetchFailure(message : String, cause : Throwable = null)              extends UnifyRssException( message, cause )
+class XmlFetchFailure(message : String, cause : Throwable = null)              extends UnifyRssException( message, cause )
 
 type FeedRefMap      = immutable.Map[Rel,Ref[immutable.Seq[Byte]]]
 type FeedEndpointMap = immutable.Map[Rel,Endpoint[Unit,Unit,String,Array[Byte],Any]]
