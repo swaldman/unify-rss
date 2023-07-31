12603e547f5c6f80f9c235e45d561d3a4fe5cfb4
Steve Waldman <swaldman@mchange.com>
2023-07-31 11:03:44 -0400

    Add sbt-ethereum blog to blog list, add new everything feed.

--- main/InterfluidityMain.scala
+++ main/InterfluidityMain.scala
@@ -11,11 +11,17 @@ object InterfluidityMain extends AbstractMain {
     new URL("https://drafts.interfluidity.com/feed/index.rss"),
     new URL("https://tech.interfluidity.com/feed/index.rss"),
     new URL("https://www.interfluidity.com/feed"),
+    new URL("https://www.sbt-ethereum.io/blog/atom.xml"),
+
   )
 
   val allBlogsAndMicroblogs = allBlogs ++ immutable.Seq(
     new URL("https://econtwitter.net/@interfluidity.rss"),
     new URL("https://fosstodon.org/@interfluidity.rss")
+  )
+
+  val everything = allBlogs ++ allBlogsAndMicroblogs ++ immutable.Seq(
+    new URL("https://github.com/swaldman.atom")
   )
 
   val AllBlogsFeed = new MergedFeed.Default(sourceUrls = allBlogs, baseName = "all-blogs", itemLimit = 25):
@@ -24,13 +30,17 @@ object InterfluidityMain extends AbstractMain {
 
   val AllBlogsAndMicroblogsFeed = new MergedFeed.Default(sourceUrls = allBlogsAndMicroblogs, baseName = "all-blogs-and-microblogs", itemLimit = 100):
     override def title(rootElems: immutable.Seq[Elem]): String = "interfluidity, all blogs and microblogs"
-    override def description(rootElems: immutable.Seq[Elem]): String = "Collects posts to all blogs by Steve Randy Waldman (interfludity), as well as posts to microblogs that syndicate by RSS."
+    override def description(rootElems: immutable.Seq[Elem]): String = "Collects posts to all blogs and microblogs by Steve Randy Waldman (interfludity), as well as posts to microblogs that syndicate by RSS."
+
+  val EverythingFeed = new MergedFeed.Default(sourceUrls = everything, baseName = "everything", itemLimit = 100):
+    override def title(rootElems: immutable.Seq[Elem]): String = "interfluidity, everything"
+    override def description(rootElems: immutable.Seq[Elem]): String = "Tracks posts to all blogs and microblogs, as well as other activity, by Steve Randy Waldman (interfludity), as well as posts to microblogs that syndicate by RSS."
 
   override val appConfig: AppConfig = AppConfig(
     serverUrl = Abs("https://www.interfluidity.com/"),
     proxiedPort = Some(8123),
     appPathServerRooted = Rooted("/unify-rss"),
-    mergedFeeds = immutable.Set(AllBlogsFeed, AllBlogsAndMicroblogsFeed),
+    mergedFeeds = immutable.Set(AllBlogsFeed, AllBlogsAndMicroblogsFeed, EverythingFeed),
     verbose = true
   )
 }
