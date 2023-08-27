e4424f0a593fb01d22fb260a03f91eb929607c50
Steve Waldman <swaldman@mchange.com>
2023-08-27 02:18:45 -0400

    Allow opml metasources to accept transformers, of the OPML, and of each (all) feeds.

--- main/com/mchange/unifyrss/config.scala
+++ main/com/mchange/unifyrss/config.scala
@@ -21,8 +21,8 @@ object SourceUrl:
 final case class SourceUrl( url : URL, transformer : Elem => Elem )
 
 object MetaSource:
-  case class OPML( opmlUrl : URL ) extends MetaSource:
-    def sourceUrls : immutable.Seq[SourceUrl] = (XML.load(opmlUrl) \\ "outline").map( _ \@ "xmlUrl" ).filter( _.nonEmpty).map( SourceUrl.apply )
+  case class OPML( opmlUrl : URL, opmlTransformer : Elem => Elem = identity, eachFeedTransformer : Elem => Elem = identity ) extends MetaSource:
+    def sourceUrls : immutable.Seq[SourceUrl] = ( opmlTransformer( XML.load(opmlUrl) ) \\ "outline").map( _ \@ "xmlUrl" ).filter( _.nonEmpty).map( feedUrl => SourceUrl( feedUrl, eachFeedTransformer ) )
 trait MetaSource:
   def sourceUrls : immutable.Seq[SourceUrl]
 end MetaSource
