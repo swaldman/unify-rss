6824fe17660e9e5fc3b6bf1e5a26e0575d2d9853
Steve Waldman <swaldman@mchange.com>
2024-05-13 02:56:05 -0400

    Bump versions for 0.0.4 final.

--- project.scala
+++ project.scala
@@ -1,6 +1,6 @@
 //> using scala "3.3.3"
 //> using dep "org.scala-lang.modules::scala-xml:2.3.0"
-//> using dep "com.mchange::audiofluidity-rss:0.0.7-SNAPSHOT"
+//> using dep "com.mchange::audiofluidity-rss:0.0.7"
 //> using dep "com.mchange::unstatic:0.2.1"
 //> using dep "dev.zio::zio:2.1.1"
 //> using dep "com.softwaremill.sttp.tapir::tapir-zio:1.10.7"
@@ -10,7 +10,7 @@
 
 //> using publish.organization "com.mchange"
 //> using publish.name "unify-rss"
-//> using publish.version "0.0.3"
+//> using publish.version "0.0.4"
 
 //> using publish.url "https://github.com/swaldman/unify-rss"
 //> using publish.developer "swaldman|Steve Waldman|https://github.com/swaldman"
